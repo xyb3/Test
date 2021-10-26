f696d934d7bdd855554647506aa622eae828a8b9
Luoky76 <1085752693@qq.com>
2021-10-26 20:59:41 +0800

    test

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+#include "Test1.cpp"
+#include "Test2.cpp"
+#include "Test3.cpp"
+#include "Test4.cpp"
+#include "Test5.cpp"
+#include "Test6.cpp"
+#include "Test8.cpp"
+int main()
+{
+	int x=0;
+	x=test1(x);
+	x=test2(x);
+	x=test3(x);
+	x=test4(x);
+	x=test5(x);
+	x=test6(x);
+	x=test8(x);
+	printf("%d",x);
+	return 0;
+}
